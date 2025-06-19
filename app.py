8cb526faa174c988b5be8df60ac47fab499ebd50
JoseMariaGarciaMendoza <20221065@uthh.edu.mx>
2025-06-18 20:48:07 -0600

    Update Model

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 logging.basicConfig(level=logging.DEBUG)
 
 # Cargar el modelo entrenado
-model = joblib.load('modelo_iris.pkl')
+model = joblib.load('modelo_bichos.pkl')
 app.logger.debug('Modelo cargado correctamente.')
 
 @app.route('/')
